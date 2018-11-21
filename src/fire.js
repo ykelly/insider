2d84249c85a78a26e2770ce79b8e24a42441eb00
yurenpang <ypang@macalester.edu>
2018-11-21 14:33:22 -0700

    React and Firebase Set Up

---
+++ src/fire.js
@@ -0,0 +1,13 @@
+import firebase from "firebase"
+
+var config = {
+  apiKey: "AIzaSyA8HgkSJZ1wXB2dOP2NFQ95ERFX0klUHTs",
+  authDomain: "airbnb-insider-81935.firebaseapp.com",
+  databaseURL: "https://airbnb-insider-81935.firebaseio.com",
+  projectId: "airbnb-insider-81935",
+  storageBucket: "airbnb-insider-81935.appspot.com",
+  messagingSenderId: "890878786128"
+};
+
+var fire = firebase.initializeApp(config);
+export default fire;
