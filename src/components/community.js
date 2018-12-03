aab1abe9e85d7daeea000602facb52709677ebac
yurenpang <ypang@macalester.edu>
2018-12-02 22:10:53 -0600

    Display Databse Articles. No Styling implemented.

--- src/components/community.js
+++ src/components/community.js
@@ -2,13 +2,60 @@ import React from 'react';
 import Header from './community/Header';
 import Footer from './community/Footer';
 import Article from './community/Article';
+import fire from './fire';
 
 class Community extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      items:[]
+    };
+  }
+
+  componentDidMount() {
+    const itemsRef = fire.database().ref('Articles');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for(let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          author: items[item].author,
+          text: items[item].text,
+          date: Date(items[item].date)
+        });
+      }
+      this.setState({
+        items: newState
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <Article />
+
+        <section className = 'display-articles'>
+          <div className = 'wrapper'>
+            <ul>
+              {
+                this.state.items.map((item) => {
+                  return(
+                    <li key={item.id}>
+                      <h3>{item.title}</h3>
+                      <h4>{item.author}</h4>
+                      <p>{item.text}</p>
+                      <p>{item.date.toString()}</p>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </section>
+
         <Footer />
       </div>
     )
--- src/components/community/Header.js
+++ src/components/community/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <header>
+      <header style={{fontSize: '2em'}}>
         <h1>Community</h1>
       </header>
     );
