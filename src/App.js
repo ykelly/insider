38ad741cc6948f503b7b0a9efc880427a753f77e
yurenpang <ypang@macalester.edu>
2018-12-02 22:13:24 -0600

    Delete base.js and redundant firebase codes.

--- src/App.js
+++ src/App.js
@@ -35,24 +35,6 @@ class App extends Component {
         messages: [] }; // <- set up react state
   }
 
-  componentWillMount(){
-    /* Create reference to messages in Firebase Database */
-    let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
-    messagesRef.on('child_added', snapshot => {
-      /* Update React state when message is added at Firebase Database */
-      let message = { text: snapshot.val(), id: snapshot.key };
-      this.setState({ messages: [message].concat(this.state.messages) });
-    })
-  }
-
-  /* Firbase writes in remote realtime database*/
-  addMessage(e) {
-      e.preventDefault(); // <- prevent form submit from reloading the page
-      /* Send the message to Firebase */
-      fire.database().ref('messages').push(this.inputEl.value);
-      this.inputEl.value = ''; // <- clear the input
-  }
-
   _updateViewport = (viewport) => {
       this.setState({viewport});
   }
