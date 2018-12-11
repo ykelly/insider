cfec6694e3b867f5ddb9308e0249ba38e808a88a
yurenpang <ypang@macalester.edu>
2018-12-10 21:02:22 -0600

    Listing marker implemented succesfully.

--- src/App.js
+++ src/App.js
@@ -3,69 +3,62 @@ import fire from "./components/fire"
 import './App.css';
 import "react-bulma-components/full";
 import 'font-awesome/css/font-awesome.min.css';
-import ReactMapGL, {Marker, Popup, NavigationControl} from 'react-map-gl';
-import CityPin from './city-pin';
-import CityInfo from './city-info';
+import MapGL, {Marker, Popup, NavigationControl, StaticMap} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Header from './components/community/Header'
 import Footer from './components/community/Footer'
 
-import CITIES from './data/cities.json';
-import LISTINGS from './data/listings.json';
-
 const TOKEN = 'pk.eyJ1IjoicndhbmcyIiwiYSI6ImNqajJ3a21hbzExZ3EzcXBnc2puNTRudWkifQ.EtOfYQEh_v4rQ0q71LAqWQ'
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-
     this.state = {
         viewport: {
-            // latitude: 47.6053202,
-            // longitude: -122.3381718,
-            // zoom: 12,
             latitude: 47.6049,
             longitude: -122.3299,
-            zoom: 15.09,
+            zoom: 12.09,
             width: "100vw",
             height: "100vh",
             bearing: 0,
-            pitch: 0
+            pitch: 0,
         },
-        popupInfo: null,
-        messages: [] }; // <- set up react state
+        hoverInfo: null
+      }; // <- set up react state
+      this._renderPopup = this._renderPopup.bind(this);
   }
-
+  
   _updateViewport = (viewport) => {
       this.setState({viewport});
   }
 
-  _renderCityMarker = (listing, index) => {
-      return (
-          <Marker
-              key={`marker-${index}`}
-              longitude={listing.longitude}
-              latitude={listing.latitude} >
-              <CityPin size={20} onClick={() => this.setState({popupInfo: listing})} />
-          </Marker>
-      );
-  }
+  _onHover = event => {
+   let hoverInfo = null;
+
+   const apartment = event.features && event.features.find(f => f.layer.id === 'listings-percent');
+   if (apartment) {
+     hoverInfo = {
+       lngLat: event.lngLat,
+       properties: apartment.properties
+     };
+   }
+   this.setState({
+     hoverInfo: hoverInfo
+   });
+ };
 
   _renderPopup() {
-      const {popupInfo} = this.state;
+   const {hoverInfo} = this.state;
 
-      return popupInfo && (
-          <Popup tipSize={5}
-                 anchor="top"
-                 longitude={popupInfo.longitude}
-                 latitude={popupInfo.latitude}
-                 closeOnClick={false}
-                 onClose={() => this.setState({popupInfo: null})} >
-              <CityInfo info={popupInfo} />
-          </Popup>
-      );
-  }
+   if (hoverInfo) {
+     return (
+       <Popup longitude={hoverInfo.lngLat[0]} latitude={hoverInfo.lngLat[1]} closeButton={false}>
+         <div>{hoverInfo.properties.neighbourhood}</div>
+       </Popup>
+     );
+   }
+   return null;
+ }
 
   render() {
       const {viewport} = this.state;
@@ -74,28 +67,23 @@ class App extends Component {
 
               <Header/>
               <div className="columns" id="columns">
-                  <div className="column is-2" id="column-one">
-                      {/*First column*/}
-                  </div>
-                  {/*<div className="column is-4" id="column-two">*/}
-                      {/*/!*Second column*!/*/}
-                  {/*</div>*/}
+                  <div className="column is-2" id="column-one"></div>
+
                   <div className="column is-6 noSelect" id="column-three">
-                      {/*Third column*/}
-                      <ReactMapGL {...this.state.viewport}
+                      <MapGL {...this.state.viewport}
                                   onViewportChange={this._updateViewport}
                                   mapStyle={'mapbox://styles/rwang2/cjooll5t33iyn2ro8jvlhzhr5'}
-                                  mapboxApiAccessToken={TOKEN}>
+                                  mapboxApiAccessToken={TOKEN}
+                                  onClick = {this._onHover}
+                                  >
 
-                          { LISTINGS.map(this._renderCityMarker) }
-
-                          {this._renderPopup()}
+                          { this._renderPopup() }
 
                           <div className="nav" style={{position: "absolute", top: 100, left: 50}}>
                               <NavigationControl onViewportChange={this._updateViewport}/>
                           </div>
 
-                      </ReactMapGL>
+                      </MapGL>
                   </div>
               </div>
               <Footer/>
@@ -103,21 +91,6 @@ class App extends Component {
           </div>
       )
   }
-
-  //Rock's firebase work
-  // render() {
-  //   return (
-  //     <form onSubmit={this.addMessage.bind(this)}>
-  //       <input type="text" ref={ el => this.inputEl = el }/>
-  //       <input type="submit"/>
-  //       <ul>
-  //         { /* Render the list of messages */
-  //           this.state.messages.map( message => <li key={message.id}>{message.text}</li> )
-  //         }
-  //       </ul>
-  //     </form>
-  //   );
-  // }
 }
 
 export default App;
