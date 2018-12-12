0385651f0e5c91eeba95291c5baa46537ab3af44
Daniel Lim <dlim@macalester.edu>
2018-12-11 21:05:17 -0600

    Added search inputs

--- src/App.js
+++ src/App.js
@@ -70,7 +70,88 @@ class App extends Component {
 
               <Header/>
               <div className="columns" id="columns">
-                  <div className="column is-2" id="column-one"></div>
+                  <div className="column is-2" id="column-one">
+                      <div id="search">
+                          <p className="is-size-2 is-uppercase has-text-weight-bold">Search</p>
+                      </div>
+                      <div id="first-search-text">
+                          <p className="is-size-6 has-text-weight-bold">Price Range</p>
+                      </div>
+                      <div className="control has-icons-left" id="first-filter">
+                          <div className="select is-rounded">
+                              <select>
+                                  <option>Select neighborhood</option>
+                                  <option>Add neighborhood</option>
+                              </select>
+                          </div>
+                          <span className="icon is-left">
+                              <i className="fa fa-map"></i>
+                          </span>
+                      </div>
+                      <div id="search-text">
+                          <p className="is-size-6 has-text-weight-bold">Zip Code</p>
+                      </div>
+                      <div className="field" id="zipcode">
+                          <p className="control has-icons-left has-icons-right">
+                              <input className="input" type="text" placeholder="Enter zip code"/>
+                              <span className="icon is-small is-left">
+                                  <i className="fa fa-map-pin"></i>
+                              </span>
+                          </p>
+                      </div>
+                      <div id="search-text">
+                          <p className="is-size-6 has-text-weight-bold">Price Range</p>
+                      </div>
+                      <div className="field" id="zipcode">
+                          <p className="control has-icons-left has-icons-right">
+                              <input className="input" type="text" placeholder="From"/>
+                              <span className="icon is-small is-left">
+                                  <i className="fa fa-dollar"></i>
+                              </span>
+                          </p>
+                      </div>
+                      <div className="field" id="zipcode">
+                          <p className="control has-icons-left has-icons-right">
+                              <input className="input" type="text" placeholder="To"/>
+                              <span className="icon is-small is-left">
+                                  <i className="fa fa-dollar"></i>
+                              </span>
+                          </p>
+                      </div>
+                      <div id="search-text">
+                          <p className="is-size-6 has-text-weight-bold">Listing Type</p>
+                      </div>
+                      <div className="control has-icons-left" id="first-filter">
+                          <div className="select is-rounded">
+                              <select>
+                                  <option>Select Listing Type</option>
+                                  <option>Add listing type</option>
+                              </select>
+                          </div>
+                          <span className="icon is-left">
+                              <i className="fa fa-home"></i>
+                          </span>
+                      </div>
+                      <div id="search-text">
+                          <p className="is-size-6 has-text-weight-bold">Guests</p>
+                      </div>
+                      <div className="control has-icons-left" id="first-filter">
+                          <div className="select is-rounded">
+                              <select>
+                                  <option>Number of guests</option>
+                                  <option>1</option>
+                                  <option>2</option>
+                                  <option>3</option>
+                                  <option>4</option>
+                                  <option>5+</option>
+
+                              </select>
+                          </div>
+                          <span className="icon is-left">
+                              <i className="fa fa-user"></i>
+                          </span>
+                      </div>
+                  </div>
 
                   <div className="column is-6 noSelect" id="column-three">
                       <MapGL {...this.state.viewport}
