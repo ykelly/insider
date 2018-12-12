7d17eb7b86869ac8bfca91d5c1b930ae0b9e13e2
yurenpang <ypang@macalester.edu>
2018-12-12 11:48:11 -0600

    About Page

--- src/components/About.js
+++ src/components/About.js
@@ -5,18 +5,53 @@ import Footer from './community/Footer';
 class About extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
-        <section class="section">
-          <div class="container">
-            <h1 class="title">About</h1>
-            <h2 class="subtitle">
-              This is the About page
-            </h2>
+      <section class="hero is-success is-fullheight">
+        <div class="hero-head">
+          <Header />
+
+        </div>
+
+        <div className="hero-body">
+          <div className="container">
+            <section>
+              <p className="title is-1">
+                About
+              </p>
+            </section>
+            <br/>
+
+            <div className="columns is-desktop">
+              <div class="column">
+                <p className="subtitle">
+                  Rock's Profile
+                </p>
+              </div>
+
+              <div class="column">
+                <p className="subtitle">
+                  Katya Profile
+                </p>
+              </div>
+
+              <div class="column">
+                <p className="subtitle">
+                  Ruotong's Profile
+                </p>
+              </div>
+
+              <div class="column">
+                <p className="subtitle">
+                  Daniel's Profile
+                </p>
+              </div>
+            </div>
           </div>
-        </section>
-        <Footer />
-      </div>
+        </div>
+
+        <div class="hero-foot">
+          <Footer />
+        </div>
+      </section>
     )
   }
 }
