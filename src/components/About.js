9e91d6eb398285018715f0e4e2daef592027a4f9
Daniel Lim <dlim@macalester.edu>
2018-12-14 16:39:55 -0600

    About page styling

--- src/components/About.js
+++ src/components/About.js
@@ -12,37 +12,90 @@ class About extends React.Component {
         </div>
 
         <div className="hero-body" id="about-page">
-          <div className="container">
-            <section>
-              <p className="title is-1">
-                About
-              </p>
-            </section>
-            <br/>
+          <div className="container" id="about-page-alignment">
+
+              <div id="about-icon" className="has-text-centered">
+                  <img src={require("./../images/team.png")}
+                       height="80px"
+                       width="80px"/>
+              </div>
+
+              <div className="is-warning" id="about-statement">
+                  <p className="title has-text-black">Outstanding Team</p>
+                  <p className="about-subtitle" id="about">We want to provide the perfect experience for both the traveler and<br/>the Airbnb host by providing them with a reasonable price point.</p>
+              </div>
 
             <div className="columns is-desktop">
-              <div class="column">
-                <p className="subtitle">
-                  Rock's Profile
-                </p>
+              <div class="column about-column">
+                  <div className="about-circle-1">
+                  </div>
+                  <div className="card about-card">
+                      <div className="card-content about-card-content">
+                          <p className="title is-5">
+                              <a className="has-text-black">Daniel Lim</a>
+                          </p>
+                          <p className="subtitle about-subtitle">
+                              Lead UI Designer
+                          </p>
+                          <p className="has-text-success is-size-7">
+                              Macalester College '20
+                          </p>
+                      </div>
+                  </div>
               </div>
 
-              <div class="column">
-                <p className="subtitle">
-                  Katya Profile
-                </p>
+              <div class="column about-column">
+                  <div className="about-circle-2">
+                  </div>
+                  <div className="card about-card">
+                      <div className="card-content about-card-content">
+                          <p className="title is-5">
+                              <a className="has-text-black">Ruotong Wang</a>
+                          </p>
+                          <p className="subtitle about-subtitle">
+                              Front-end Developer
+                          </p>
+                          <p className="has-text-success is-size-7">
+                              Macalester College '19
+                          </p>
+                      </div>
+                  </div>
               </div>
 
-              <div class="column">
-                <p className="subtitle">
-                  Ruotong's Profile
-                </p>
+              <div class="column about-column">
+                  <div className="about-circle-3">
+                  </div>
+                  <div className="card about-card">
+                      <div className="card-content about-card-content">
+                          <p className="title is-5">
+                              <a className="has-text-black">Rock Pang</a>
+                          </p>
+                          <p className="subtitle about-subtitle">
+                              Back-end Developer
+                          </p>
+                          <p className="has-text-success is-size-7">
+                              Macalester College '18
+                          </p>
+                      </div>
+                  </div>
               </div>
 
-              <div class="column">
-                <p className="subtitle">
-                  Daniel's Profile
-                </p>
+              <div class="column about-column">
+                  <div className="about-circle-4">
+                  </div>
+                  <div className="card about-card">
+                      <div className="card-content about-card-content">
+                          <p className="title is-5">
+                              <a className="has-text-black">Katya Kelly</a>
+                          </p>
+                          <p className="subtitle about-subtitle">
+                              Database Manager
+                          </p>
+                          <p className="has-text-success is-size-7">
+                              Macalester College '20
+                          </p>
+                      </div>
+                  </div>
               </div>
             </div>
           </div>
