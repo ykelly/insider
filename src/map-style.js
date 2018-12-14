3d7ee4f43848c2f78216a608e3d568e72ee2cbfe
ruotongw <rwang2@macalester.edu>
2018-12-14 00:10:09 -0600

    can get style json now

--- src/map-style.js
+++ src/map-style.js
@@ -1,28 +1,93 @@
 import {fromJS} from 'immutable';
-import {Window as URL} from "mapbox-gl";
+import React from 'react';
 
+const MAP_STYLE = "https://api.mapbox.com/styles/v1/mapbox/streets-v10?access_token=pk.eyJ1IjoicndhbmcyIiwiYSI6ImNqajJ3a21hbzExZ3EzcXBnc2puNTRudWkifQ.EtOfYQEh_v4rQ0q71LAqWQ";
+var style = [];
 
-const url = require('url');
-const myURL =
-    new URL("@mapbox://styles/rwang2/cjpj682np42vh2rr6zz04eddu");
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
 
-const MAP_STYLE = require(myURL);
-const stylesClient = MAP_STYLE({ accessToken: 'pk.eyJ1IjoicndhbmcyIiwiYSI6ImNqajJ3a21hbzExZ3EzcXBnc2puNTRudWkifQ.EtOfYQEh_v4rQ0q71LAqWQ'});
-var style = {};
+getJSON(MAP_STYLE,
+function(err, data) {
+  if (err !== null) {
+    alert('Something went wrong: ' + err);
+  } else {
+    style = data;
+    console.log(style);
+    console.log(data);
+  }
+});
+  // .catch((error) => {
+  //   console.log(error);
+  // })
+// class MapS extends React.Component {
+//   constructor() {
+//     this.state = {
+//       style: []
+//     }
+//   }
+//
+//   componentDidMount() {
+//     fetch(MAP_STYLE)
+//       .then((response) => response.json())
+//       .then((responseJson) => {
+//         this.state.style = responseJson
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       })
+//   }
+//
+//   render() {
+//     return(
+//       {style = this.state.style;}
+//     )
+//   }
+// }
 
-stylesClient
-    .getStyle({
-        styleId: 'Seattle - New'
-    })
-    .send()
-    .then(response => {
-        style = response.body;
-    });
+// function getJson() {
+//   fetch(MAP_STYLE)
+//     .then((response) => response.json())
+//     .then((responseJson) => {
+//       var ls = responseJson.map()
+//       return responseJson;
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+// }
+//
+// const style = getJson();
+// console.log(style);
+// const stylesClient = MAP_STYLE({ accessToken: 'pk.eyJ1IjoicndhbmcyIiwiYSI6ImNqajJ3a21hbzExZ3EzcXBnc2puNTRudWkifQ.EtOfYQEh_v4rQ0q71LAqWQ'});
+//
+// var style = {};
 
+// stylesClient
+//     .getStyle({
+//         styleId: 'Seattle - New'
+//     })
+//     .send()
+//     .then(response => {
+//         style = response.body;
+//     });
+console.log(style);
 // Make a copy of the map style
 const mapStyle = {...style,
-                  sources: {...MAP_STYLE.sources},
-                  layers: MAP_STYLE.layers.slice()
+                  sources: {...style.sources},
+                  layers: style.layers.slice()
                  };
 
 
